00a3a8a897cbc5daf4bd6376be3972d5daf6f6b3
Anurag Sharma <imanurag11sep@gmail.com>
2026-06-09 08:53:50 +0530

    Add calculator.cpp to basics folder

---
+++ basics/calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+    cout << "Enter First Number: \n";
+    cin >> a;
+    cout << "Enter Second Number: \n";
+    cin >> b;
+    cout << "The sum is : " << a + b << "\n";
+    cout << "The difference is : " << a - b << "\n";
+    cout << "The product is : " << a * b << "\n";
+    cout << "The division is : " << a / b << "\n";
+    return 0;
+}
