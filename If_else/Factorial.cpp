6b00efc0b07b55d6a9074ef6f48d76966471c344
Anurag Sharma <imanurag11sep@gmail.com>
2026-06-13 12:03:42 +0530

    Implement iterative factorial calculation
    Added iterative function to calculate factorial.

--- If_else/Factorial.cpp
+++ If_else/Factorial.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+
+// Using unsigned long long to handle larger factorial results
+unsigned long long getFactorialIterative(int n) {
+    if (n < 0) {
+        return 0; // Factorial for negative numbers is not defined
+    }
+    
+    unsigned long long factorial = 1;
+    for (int i = 1; i <= n; ++i) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+int main() {
+    int num;
+    std::cout << "Enter a positive integer: ";
+    std::cin >> num;
+
+    if (num < 0) {
+        std::cout << "Error! Factorial of a negative number doesn't exist." << std::endl;
+    } else {
+        std::cout << "Factorial of " << num << " = " << getFactorialIterative(num) << std::endl;
+    }
+
+    return 0;
+}
 
